0cce55337fe64699a8ac3bf4eef5c10908f7c435
leandro.yoshikay <leandro.yoshikay@quality.com.br>
2018-08-27 16:43:51 -0300

    fix

--- app/scripts/components/parallax.js
+++ app/scripts/components/parallax.js
@@ -1,28 +1,4 @@
-	react_to_window();
-        
-	//only acstivate stellar for window widths above or equal to 1024
-    var stellarActivated = false;
-    
-    $(window).on('resize', function() {
-        react_to_window();
-    });
-    
-    function react_to_window() {
-        if ($(window).width() <= 1024) {
-            if (stellarActivated === true) {
-                $(window).data('plugin_stellar').destroy();
-                stellarActivated = false;
-            }
-        } else {
-            if (stellarActivated === false) {
 
-                $.stellar({
-                	horizontalScrolling: false,
-					responsive: true,
-               });
-                
-                $(window).data('plugin_stellar').init();
-                stellarActivated = true;
-            }
-        }
-    }
+$('.bg-1, .bg-2, .bg-3, .bg-4, #home').imageScroll({
+    mediaHeight: 200
+});
