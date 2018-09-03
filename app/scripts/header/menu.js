b91af6be4dd01da8436d1faf9ccf9c01ed996c7a
leandro.yoshikay <leandro.yoshikay@quality.com.br>
2018-09-03 18:24:09 -0300

    fix

--- app/scripts/header/menu.js
+++ app/scripts/header/menu.js
@@ -12,3 +12,16 @@ $("#nav_list li").click(function(){
     $('.navbar-toggle').click();
   }
 });
+
+$( '.scroll-event-inhoshi' ).click( eventScroll )
+
+function eventScroll(){
+	var altura = $('.navbar-header').offset().top;
+  var scrollUser = $( document ).scrollTop();
+  var isClassMenu = $( '.sticky-wrapper' ).hasClass( 'is-sticky' );
+  
+	if( !isClassMenu && altura > scrollUser ){
+		$('html, body').animate({scrollTop:altura}, 'slow');
+  }
+}
+
