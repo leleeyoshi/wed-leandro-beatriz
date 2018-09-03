32e22738edfd32fdf0aa9fdbc61c35d288c55f7c
leandro.yoshikay <leandro.yoshikay@quality.com.br>
2018-09-03 17:34:32 -0300

    fix

--- app/scripts/header/menu.js
+++ app/scripts/header/menu.js
@@ -2,6 +2,11 @@ $(window).on('load', function(){
   $('#nav-sticky-wrapper').sticky({ topSpacing: 0 });
 });
 
+$('#nav_list').onePageNav({
+    currentClass: 'active'
+});
+
+
 $("#nav_list li").click(function(){
   if($('.navbar-toggle').is(':visible')){
     $('.navbar-toggle').click();
