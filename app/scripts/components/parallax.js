287dc748f4f259813eb8fdfa1ef5c0c7de1f4ae6
leandro.yoshikay <leandro.yoshikay@quality.com.br>
2018-09-10 19:02:42 -0300

    fix

--- app/scripts/components/parallax.js
+++ app/scripts/components/parallax.js
@@ -1,4 +1,4 @@
  $('.bg-1, .bg-2, .bg-3, #home').imageScroll();
- $('.bg-4').imageScroll({
-        holderClass: 'footer'
-    });
+//  $('.bg-4').imageScroll({
+//         holderClass: 'footer'
+//     });
