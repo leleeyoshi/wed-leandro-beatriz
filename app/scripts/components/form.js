17ff975dd8c0876ae48bf3945a2c1c53b1c8406c
leandro.yoshikay <leandro.yoshikay@quality.com.br>
2018-12-12 18:28:44 -0200

    Retirando form

--- app/scripts/components/form.js
+++ app/scripts/components/form.js
@@ -1,81 +1,81 @@
-const modalOpen = document.getElementsByClassName('modal__wrap');
-const modalContent = document.getElementsByClassName('modal__content');
-const messageModal = document.querySelector('.modal__wrap .append');
-const responseForm = document.getElementsByClassName('responseForm');
-const cadastrarSubmit = document.getElementsByClassName('cadastrarSubmit');
+// const modalOpen = document.getElementsByClassName('modal__wrap');
+// const modalContent = document.getElementsByClassName('modal__content');
+// const messageModal = document.querySelector('.modal__wrap .append');
+// const responseForm = document.getElementsByClassName('responseForm');
+// const cadastrarSubmit = document.getElementsByClassName('cadastrarSubmit');
 
-const sucessMessage = 
-    '<div class="append modal__sucess">'+
-        '<span class="modal__close" id="modal-close" onclick="resetForm()">x</span> Sua confirmação foi recebida com sucesso. Qualquer dúvida entre em contato pelo e-mail: contato@puntacanabiaeleandro.com.br.'+
-    '</div>';
+// const sucessMessage = 
+//     '<div class="append modal__sucess">'+
+//         '<span class="modal__close" id="modal-close" onclick="resetForm()">x</span> Sua confirmação foi recebida com sucesso. Qualquer dúvida entre em contato pelo e-mail: contato@puntacanabiaeleandro.com.br.'+
+//     '</div>';
 
-document.getElementById('cadastrarSubmit').addEventListener('click', function(event) {
-    event.preventDefault();
-    const isValidForm = document.getElementById('cadastrarForm').checkValidity();
-    form.validaFormSubmit();
+// document.getElementById('cadastrarSubmit').addEventListener('click', function(event) {
+//     event.preventDefault();
+//     const isValidForm = document.getElementById('cadastrarForm').checkValidity();
+//     form.validaFormSubmit();
 
-    if(isValidForm){
-        cadastrarSubmit[0].disabled = true;
-        responseForm[0].innerText = 'Enviando ...';
-        responseForm[0].style.display = 'hide';
-        sendForm();
-    }
+//     if(isValidForm){
+//         cadastrarSubmit[0].disabled = true;
+//         responseForm[0].innerText = 'Enviando ...';
+//         responseForm[0].style.display = 'hide';
+//         sendForm();
+//     }
 
-});
+// });
 
-function sendForm() {
-    const serializedForm = document.getElementById('cadastrarForm');
-    const _data = toJSONString(serializedForm);
-    console.log(_data);
-    axios({
-            url: 'http://api.puntacanabiaeleandro.com.br/api/cadastrar',
-            method: 'POST',
-            data: _data,
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(function(response) {
+// function sendForm() {
+//     const serializedForm = document.getElementById('cadastrarForm');
+//     const _data = toJSONString(serializedForm);
+//     console.log(_data);
+//     axios({
+//             url: 'http://api.puntacanabiaeleandro.com.br/api/cadastrar',
+//             method: 'POST',
+//             data: _data,
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             }
+//         }).then(function(response) {
 
-            if (response.data.data == 'ok') {
-                modalOpen[0].style.display = 'flex';
-                modalContent[0].style.display = 'block';
-                var div = document.createElement('div');
-                div.innerHTML = sucessMessage.trim();
-                modalContent[0].appendChild(div);
-                $('body').css('overflow', 'hidden');
-                cadastrarSubmit[0].disabled = false;
-            }
+//             if (response.data.data == 'ok') {
+//                 modalOpen[0].style.display = 'flex';
+//                 modalContent[0].style.display = 'block';
+//                 var div = document.createElement('div');
+//                 div.innerHTML = sucessMessage.trim();
+//                 modalContent[0].appendChild(div);
+//                 $('body').css('overflow', 'hidden');
+//                 cadastrarSubmit[0].disabled = false;
+//             }
 
-        })
-        .catch(function(e) {
+//         })
+//         .catch(function(e) {
 
-            console.log(e.response.data.data);
-            responseForm[0].innerText = "Não foi possível cadastrar no momento, tente novamente ou entre em contato pelo e-mail contato@puntacanabiaeleandro.com.br";
-            responseForm[0].style.display = 'block';
-            cadastrarSubmit[0].disabled = false;
-        });
+//             console.log(e.response.data.data);
+//             responseForm[0].innerText = "Não foi possível cadastrar no momento, tente novamente ou entre em contato pelo e-mail contato@puntacanabiaeleandro.com.br";
+//             responseForm[0].style.display = 'block';
+//             cadastrarSubmit[0].disabled = false;
+//         });
 
-    return false;
-}
+//     return false;
+// }
 
-function toJSONString(form) {
-    var obj = {};
-    var elements = form.querySelectorAll('input, select, textarea');
+// function toJSONString(form) {
+//     var obj = {};
+//     var elements = form.querySelectorAll('input, select, textarea');
 
-    for (var i = 0; i < elements.length; ++i) {
-        var element = elements[i];
-        obj[element.name] = element.value;
-    }
+//     for (var i = 0; i < elements.length; ++i) {
+//         var element = elements[i];
+//         obj[element.name] = element.value;
+//     }
 
-    return JSON.stringify(obj);
-}
+//     return JSON.stringify(obj);
+// }
 
-function resetForm() {
-    setTimeout(function() {
-        window.location.reload();
-    }, 100);
-    $('body').css('overflow', 'auto');
-}
+// function resetForm() {
+//     setTimeout(function() {
+//         window.location.reload();
+//     }, 100);
+//     $('body').css('overflow', 'auto');
+// }
 
 var ios = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 
--- app/scripts/components/required.js
+++ app/scripts/components/required.js
@@ -1,56 +1,56 @@
-const elem = document.querySelectorAll('input[required], select[required]');
+// const elem = document.querySelectorAll('input[required], select[required]');
 
-const isvalid = {
-    checkValidity: 'nok'
-}
+// const isvalid = {
+//     checkValidity: 'nok'
+// }
 
-var verifyCallback = function(response) {
-    if (response.length) {
-        captcha.classList.remove('has-error');
-    }
-};
+// var verifyCallback = function(response) {
+//     if (response.length) {
+//         captcha.classList.remove('has-error');
+//     }
+// };
 
-class Required {
-    validaFormSubmit() {
-        const eventT = document.createEvent('HTMLEvents');
-        eventT.initEvent('blur', true, false);
+// class Required {
+//     validaFormSubmit() {
+//         const eventT = document.createEvent('HTMLEvents');
+//         eventT.initEvent('blur', true, false);
 
-        for (var i = 0; i < elem.length; i++) {
-            elem[i].dispatchEvent(eventT);
-        }
-    }
+//         for (var i = 0; i < elem.length; i++) {
+//             elem[i].dispatchEvent(eventT);
+//         }
+//     }
 
-    requiredElem() {
+//     requiredElem() {
 
-        for (var i = 0; i < elem.length; i++) {
-            elem[i].addEventListener('blur', function() {
+//         for (var i = 0; i < elem.length; i++) {
+//             elem[i].addEventListener('blur', function() {
 
-                const checkValid = document.getElementById('cadastrarForm').checkValidity();
+//                 const checkValid = document.getElementById('cadastrarForm').checkValidity();
 
-                const msg = this.parentNode.getElementsByClassName('with-errors')[0];
+//                 const msg = this.parentNode.getElementsByClassName('with-errors')[0];
 
-                if (!this.checkValidity()) {
+//                 if (!this.checkValidity()) {
 
-                    this.classList.add('has-error');
-                    msg.style.display = 'block';
+//                     this.classList.add('has-error');
+//                     msg.style.display = 'block';
 
-                    // isvalid.checkValidity = 'nok';
+//                     // isvalid.checkValidity = 'nok';
 
-                } else {
+//                 } else {
 
-                    this.classList.remove('has-error');
-                    msg.style.display = 'none';
+//                     this.classList.remove('has-error');
+//                     msg.style.display = 'none';
 
-                }
+//                 }
 
-                if (checkValid) {
-                    isvalid.checkValidity = 'ok';
-                }
-            });
-        }
-    }
-}
+//                 if (checkValid) {
+//                     isvalid.checkValidity = 'ok';
+//                 }
+//             });
+//         }
+//     }
+// }
 
-const form = new Required();
+// const form = new Required();
 
-form.requiredElem();
+// form.requiredElem();
